1ae2b6d7eb2df21b90fb9ffddf730448d984c04d
Will <will-blk@hotmail.com>
2024-06-14 13:17:15 -0300

    Add rubocop-rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Apply rubocop to generated files
+  config.generators.after_generate do |files|
+    parsable_files = files.filter { |file| file.end_with?('.rb') }
+    unless parsable_files.empty?
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
+  end
 end
